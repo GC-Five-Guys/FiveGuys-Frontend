21a0c1dcc5d984a385a4e968a5f826daebcce643
guingguing <kimjinseo0402@gmail.com>
2026-05-27 22:50:07 +0900

    feat: 3D 태그 그래프 뷰 추가

--- obsidian_clone/client/src/App.tsx
+++ obsidian_clone/client/src/App.tsx
@@ -6,6 +6,7 @@ import { Tabs } from './components/Tabs';
 import { StatusBar } from './components/StatusBar';
 import { RightSidebar } from './components/RightSidebar';
 import { TagSearchResultsView } from './components/TagSearchResultsView';
+import { GraphView } from './components/GraphView';
 import {
   countTopTags,
   fetchNoteTagIndex,
@@ -250,21 +251,20 @@ function App() {
         </div>
       </nav>
 
-      {activeView === 'file' ? (
-        <>
-          <Sidebar
-            treeData={treeData}
-            recentNotes={recentNotes}
-            selectedFolderPath={selectedFolderPath}
-            currentPath={currentPath}
-            setSelectedFolderPath={setSelectedFolderPath}
-            openNote={openNote}
-            handleCreateNote={handleCreateNote}
-            handleCreateFolder={handleCreateFolder}
-            handleDelete={handleDelete}
-          />
+      <Sidebar
+        treeData={treeData}
+        recentNotes={recentNotes}
+        selectedFolderPath={selectedFolderPath}
+        currentPath={currentPath}
+        setSelectedFolderPath={setSelectedFolderPath}
+        openNote={openNote}
+        handleCreateNote={handleCreateNote}
+        handleCreateFolder={handleCreateFolder}
+        handleDelete={handleDelete}
+      />
 
-          <main id="main-content">
+      {activeView === 'file' ? (
+        <main id="main-content">
             <Tabs
               openTabs={openTabs}
               currentPath={currentPath}
@@ -301,24 +301,31 @@ function App() {
               )
             )}
           </main>
-        </>
       ) : (
         <main id="main-content">
-          <div className="placeholder-view">그래프 모드 준비 중...</div>
+          <GraphView
+            notes={tagIndex}
+            onOpenNote={(path) => {
+              openNote(path);
+              setActiveView('file');
+            }}
+          />
         </main>
       )}
 
-      <RightSidebar
-        selectedTagType={selectedTagType}
-        searchQuery={tagSearchQuery}
-        topTags={topTags}
-        calendarNotes={calendarNotes}
-        onTagTypeChange={setSelectedTagType}
-        onSearchQueryChange={setTagSearchQuery}
-        onSearchSubmit={() => runTagSearch()}
-        onTopTagClick={(type, label) => runTagSearch(type, label)}
-        onCalendarDateClick={handleCalendarDateClick}
-      />
+      {activeView === 'file' && (
+        <RightSidebar
+          selectedTagType={selectedTagType}
+          searchQuery={tagSearchQuery}
+          topTags={topTags}
+          calendarNotes={calendarNotes}
+          onTagTypeChange={setSelectedTagType}
+          onSearchQueryChange={setTagSearchQuery}
+          onSearchSubmit={() => runTagSearch()}
+          onTopTagClick={(type, label) => runTagSearch(type, label)}
+          onCalendarDateClick={handleCalendarDateClick}
+        />
+      )}
 
       <StatusBar saveStatus={saveStatus} />
 
