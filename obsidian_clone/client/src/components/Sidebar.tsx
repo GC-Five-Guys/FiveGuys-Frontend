cd9f6c80ab33895bf5fb957fdcf13c3e2c818887
guingguing <kimjinseo0402@gmail.com>
2026-05-28 16:47:16 +0900

    design: 왼쪽 상단 로고 추가

--- obsidian_clone/client/src/components/Sidebar.tsx
+++ obsidian_clone/client/src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import appIcon from '../assets/yggdrasil_icon_transparent.svg';
 
 interface SidebarProps {
   treeData: any[];
@@ -61,6 +62,14 @@ export const Sidebar: React.FC<SidebarProps> = ({
 
   return (
     <aside id="file-sidebar">
+      <div className="app-brand">
+        <img src={appIcon} alt="Yggdrasil 로고" />
+        <div>
+          <strong>Yggdrasil</strong>
+          <span>나만의 지식 일기</span>
+        </div>
+      </div>
+
       <div className="sidebar-section">
         <div className="section-header">
           <h3>최근 노트</h3>
